58c0d4d784b6d2fbe29e77766081f49a951cef85
Joseph Riddle <joeriddles10@gmail.com>
2024-02-02 19:41:46 -0800

    Init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Late Night Woodworking
+
+[YouTube](https://www.youtube.com/@LateNightWoodworking)
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline mb-3">
+    Late Night Woodworking
+  </h1>
+
+  <button class="btn btn-primary">
+    Get 🪵
+  </button>
+</template>
+
+<style scoped>
+</style>
