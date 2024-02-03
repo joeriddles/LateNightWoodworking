2afd91279030976d325daa5fb11a6778c2d083cf
Joseph Riddle <joeriddles10@gmail.com>
2024-02-02 20:37:11 -0800

    Add Carousel component

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,8 @@
     <h1 className="text-3xl font-bold underline">
       Late Night Woodworking
     </h1>
-  
-    <div class="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box max-w-[800px] max-h-[400px]">
-      <div id="item1" class="carousel-item relative w-full">
-        <img :src="table1Url" class="w-full object-cover rounded-box" />
-        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#item3" class="btn btn-sm btn-ghost btn-circle">❮</a> 
-          <a href="#item2" class="btn btn-sm btn-ghost btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="item2" class="carousel-item relative w-full">
-        <img :src="table2Url" class="w-full object-cover rounded-box" />
-        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#item1" class="btn btn-sm btn-ghost btn-circle">❮</a> 
-          <a href="#item3" class="btn btn-sm btn-ghost btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="item3" class="carousel-item relative w-full">
-        <img :src="table3Url" class="w-full object-cover rounded-box" />
-        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#item2" class="btn btn-sm btn-ghost btn-circle">❮</a> 
-          <a href="#item1" class="btn btn-sm btn-ghost btn-circle">❯</a>
-        </div>
-      </div>
-    </div>
+
+    <Carousel :imageUrls="imageUrls" />
   
     <div>
       <a href="https://www.youtube.com/@LateNightWoodworking" target="_blank" class="btn btn-primary">
@@ -40,11 +18,11 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import Carousel from './components/Carousel.vue';
 
-const table1Url = ref(new URL('./assets/table1.webp', import.meta.url).href)
-const table2Url = ref(new URL('./assets/table2.webp', import.meta.url).href)
-const table3Url = ref(new URL('./assets/table3.webp', import.meta.url).href)
+const imageUrls = ref([
+  new URL('./assets/table1.webp', import.meta.url).href,
+  new URL('./assets/table2.webp', import.meta.url).href,
+  new URL('./assets/table3.webp', import.meta.url).href,
+])
 </script>
-
-<style scoped>
-</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box max-w-[800px] max-h-[400px]">
+    <template v-for="(imageUrl, index) in props.imageUrls" :key="imageUrl">
+      <div :id="`item${index}`" class="carousel-item relative w-full">
+        <img  :src="imageUrl" class="w-full object-cover rounded-box" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a :href="getPrevId(index)" class="btn btn-sm btn-ghost btn-circle">❮</a> 
+          <a :href="getNextId(index)" class="btn btn-sm btn-ghost btn-circle">❯</a>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    imageUrls: string[]
+}>();
+
+function getPrevId(index: number): string {
+  let prevIndex = index - 1;
+  if (prevIndex < 0) {
+    prevIndex = props.imageUrls.length - 1;
+  }
+  return `#item${prevIndex}`;
+}
+
+function getNextId(index: number): string {
+  let nextIndex = index + 1;
+  if (nextIndex >= props.imageUrls.length) {
+    nextIndex = 0;
+  }
+  return `#item${nextIndex}`;
+}
+
+</script>
